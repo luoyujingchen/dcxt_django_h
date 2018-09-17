7d31f9943b11ca87bf9b22df5fc14e1b22bc53ba
hl <huanglei0814@foxmail.com>
2018-09-18 00:05:07 +0800

    init img model

--- dcxt/models.py
+++ dcxt/models.py
@@ -10,3 +10,9 @@ class Dish(models.Model):
     pictures = models.CharField(max_length=800)#图片请按照存储名规则
     weight = models.IntegerField(default=1)
     introduction = models.TextField(blank=True)
+
+
+class Img(models.Model):
+    img_url = models.ImageField()
+    img_info = models.CharField(max_length=420,blank=True)
+    img_by = models.ForeignKey(to=Dish, on_delete=models.CASCADE)
