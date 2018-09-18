1dd8ff6c3024f93be5c1cff25b95b452233aeec2
hl <huanglei0814@foxmail.com>
2018-09-19 00:12:00 +0800

    addDish page

--- WebClient/urls.py
+++ WebClient/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from WebClient import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('RESTfulApi.urls')),
-    path('img/',include('testmm.urls')),
-    path('web/',include('WebClient.urls'))
+    path('addDish/',views.go_add_dish)
 ]
--- dcxt_django/settings.py
+++ dcxt_django/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'RESTfulApi',
     'dcxt',
+    'WebClient',
     'testmm',
 ]
 
